b447ef1375cba6468900ebc66a48555a0d505340
dylan-murphy <dylan.murphy1@mycit.ie>
2016-09-25 13:18:14 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestPrimeFactor_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const long number = 600851475143; //Number given
+            int max = 0; //Starting answer.
+            for (int i = 2; i < Math.Sqrt(number); i++)
+            {
+                if (number%i == 0 && PrimeTest(i)) //Test for even division & prime
+                {
+                    max = i; // new max
+                }
+
+            }
+            Console.WriteLine("-----");
+            Console.WriteLine(max);
+            Console.WriteLine("-----");
+            Console.ReadKey();
+        }
+
+        //Prime Test using modulus.
+        private static bool PrimeTest(int n)
+        {
+            for (int i = 2; i <= n/2; i++)
+            {
+                if(n%i == 0)
+                {
+                    return false;
+                }
+
+            }
+            return true;
+        }
+    }
+}
